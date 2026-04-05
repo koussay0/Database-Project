7460c184fc8a62f05ce525f1945b213e2134e770
Koussay Jaballah <koussayjaballah01@gmail.com>
2026-04-05 17:29:49 -0400

    crud updated and started a transaction

--- db_design/crud.sql
+++ db_design/crud.sql
@@ -1,24 +1,27 @@
 --- store procedures for instructor table following CRUD
 -- insert an instructor
-CREATE PROCEDURE create_instructor
-    @instructor_id INT,
-    @first_name NVARCHAR(50),
-    @last_name NVARCHAR(50),
-    @email NVARCHAR(100),
-    @salary DECIMAL(18, 2)
-AS
+DELIMETER //
+CREATE PROCEDURE create_instructor(
+    IN instructor_id1 INT,
+    IN first_name1 VARCHAR(50),
+    IN last_name1 VARCHAR(50),
+    IN email1 VARCHAR(100),
+    IN salary1 DECIMAL(12, 2))
 BEGIN
     INSERT INTO instructor (instructor_id, first_name, last_name, email, salary)
-    VALUES (@instructor_id, @first_name, @last_name, @email, @salary)
+    VALUES (instructor_id1, first_name1, last_name1, email1, salary1)
 END
+//
+DELIMETER ;
+
 -- update an instructor
-CREATE PROCEDURE update_instructor
-    @instructor_id INT,
-    @first_name NVARCHAR(50),
-    @last_name NVARCHAR(50),
-    @email NVARCHAR(100),
-    @salary DECIMAL(18, 2)
-AS
+DELIMETER //
+CREATE PROCEDURE update_instructor(
+    IN instructor_id INT,
+    IN first_name VARCHAR(50),
+    IN last_name VARCHAR(50),
+    IN email VARCHAR(100),
+    IN salary DECIMAL(12, 2))
 BEGIN
     UPDATE instructor
     SET first_name = @first_name,
@@ -27,106 +30,154 @@ BEGIN
         salary = @salary
     WHERE instructor_id = @instructor_id
 END
+//
+DELIMETER ;
 -- delete an instructor
-CREATE PROCEDURE delete_instructor
-    @instructor_id INT
+DELIMETER //
+CREATE PROCEDURE delete_instructor(
+    IN instructor_id1 INT)
 AS
 BEGIN
     DELETE FROM instructor
-    WHERE instructor_id = @instructor_id
+    WHERE instructor_id = instructor_id1
 END
+//
+DELIMETER ;
 -- show an instructor
-CREATE PROCEDURE retrieve_instructor
-    @instructor_id INT
-AS
+DELIMETER //
+CREATE PROCEDURE retrieve_instructor(
+    IN instructor_id1 INT)
 BEGIN
     SELECT instructor_id, first_name, last_name, email, salary
     FROM instructor
-    WHERE instructor_id = @instructor_id
+    WHERE instructor_id = instructor_id1
 END
-
+//
+DELIMETER ;
 --- store procedures for student table following CRUD
-CREATE PROCEDURE create_student
-    @student_id INT,
-    @first_name NVARCHAR(50),
-    @last_name NVARCHAR(50),
-    @email NVARCHAR(100)
-AS
+DELIMETER //
+CREATE PROCEDURE create_student(
+    IN student_id1 INT,
+    IN first_name1 VARCHAR(50),
+    IN last_name1 VARCHAR(50),
+    IN email1 VARCHAR(100))
 BEGIN
     INSERT INTO student (STUDENT_ID, FIRST_NAME, LAST_NAME, EMAIL)
-    VALUES (@student_id, @first_name, @last_name, @email)
+    VALUES (student_id1, first_name1, last_name1, email1)
 END
-
-CREATE PROCEDURE update_student
-    @student_id INT,
-    @first_name NVARCHAR(50),
-    @last_name NVARCHAR(50),
-    @email NVARCHAR(100)
-AS
+//
+DELIMETER ;
+-- update a student
+CREATE PROCEDURE update_student(
+    IN student_id1 INT,
+    IN first_name1 VARCHAR(50),
+    IN last_name1 VARCHAR(50),
+    IN email1 VARCHAR(100))
 BEGIN
     UPDATE student
-    SET FIRST_NAME = @first_name,
-        LAST_NAME = @last_name,
-        EMAIL = @email
-    WHERE STUDENT_ID = @student_id
+    SET FIRST_NAME = first_name1,
+        LAST_NAME = last_name1,
+        EMAIL = email1
+    WHERE STUDENT_ID = student_id1
 END
+//
+DELIMITER  ;
 
-CREATE PROCEDURE delete_student
-    @student_id INT
-AS
+-- delete a student
+DELIMITER  //
+CREATE PROCEDURE delete_student(
+    IN student_id1 INT)
 BEGIN
     DELETE FROM student
-    WHERE STUDENT_ID = @student_id
+    WHERE STUDENT_ID = student_id1
 END
+//
+DELIMITER  ;
 
-CREATE PROCEDURE retrieve_student
-    @student_id INT
-AS
+-- show a student
+DELIMETER //
+CREATE PROCEDURE retrieve_student(
+    IN student_id1 INT)
 BEGIN
     SELECT STUDENT_ID, FIRST_NAME, LAST_NAME, EMAIL
     FROM student
-    WHERE STUDENT_ID = @student_id
+    WHERE STUDENT_ID = student_id1
 END
+//
+DELIMETER  ;
 
 --- store procedures for section table following CRUD
-CREATE PROCEDURE create_section
-    @section_id INT,
-    @semester INT,
-    @year INT,
-    @capacity INT
-AS
+DELIMETER //
+CREATE PROCEDURE create_section(
+    IN section_id1 INT,
+    IN semester1 INT,
+    IN year1 INT,
+    IN capacity1 INT)
 BEGIN 
-    INSERT INTO section(section_id, semester, year, capacity)
-    VALUES (@section_id, @semester, @year, @capacity)
+    INSERT INTO section(section_id, semester,year, capacity)
+    VALUES (section_id1, semester1, year1, capacity1)
 END
-
-CREATE PROCEDURE update_section
-    @section_id INT,
-    @semester INT,
-    @year INT,
-    @capacity INT
-AS
+//
+DELIMETER ;
+-- update a section
+CREATE PROCEDURE update_section(
+    IN section_id1 INT,
+    IN semester1 INT,
+    IN year1 INT,
+    IN capacity1 INT)
 BEGIN 
     UPDATE section
-    SET semester = @semester,
-        year = @year,
-        capacity = @capacity
-    WHERE section_id = @section_id
+    SET semester = semester1,
+        year = year1,
+        capacity = capacity1
+    WHERE section_id = section_id1
 END
-
-CREATE PROCEDURE delete_section
-    @section_id INT
-AS
+//
+DELIMETER ;
+-- delete a section
+CREATE PROCEDURE delete_section(
+    IN section_id1 INT)
 BEGIN 
     DELETE FROM section
     WHERE section_id = @section_id
 END
-
-CREATE PROCEDURE retrieve_section
-    @section_id INT
-AS
+//
+DELIMETER ;
+-- show a section
+CREATE PROCEDURE retrieve_section(
+    IN section_id1 INT)
 BEGIN
     SELECT section_id, semester, year, capacity
     FROM section
-    WHERE section_id = @section_id
+    WHERE section_id = section_id1
+END
+//
+DELIMETER ;
+
+
+--- Transactions starts here
+-- enrol in a class
+CREATE PROCEDURE enrol_in_class(
+    IN student_id1 INT,
+    IN section_id1 INT)
+BEGIN
+    BEGIN TRANSACTION
+    -- check if the section has capacity
+    IF (SELECT capacity FROM section WHERE section_id = @section_id) > 0
+    BEGIN
+        -- insert into enrollment table
+        INSERT INTO enrollment (student_id, section_id)
+        VALUES (@student_id, @section_id)
+
+        -- decrease the capacity of the section by 1
+        UPDATE section
+        SET capacity = capacity - 1
+        WHERE section_id = @section_id
+        COMMIT TRANSACTION
+    END
+    ELSE
+    BEGIN
+        ROLLBACK TRANSACTION
+        PRINT 'Section is full. Cannot enroll.'
+    END
 END
