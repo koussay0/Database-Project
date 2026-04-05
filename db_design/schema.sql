54912c3905fa1e88b5e5d7fe90481e303a82f6f3
Khalia Dogan <kdogan@kent.edu>
2026-04-05 15:18:23 -0400

    Expand grade enum options in enrollments table
    In the original database they had pluses and minuses for grades so i added them in as options

--- db_design/schema.sql
+++ db_design/schema.sql
@@ -138,7 +138,7 @@ CREATE TABLE enrollments (
     enrollment_id INT AUTO_INCREMENT PRIMARY KEY,
     student_id INT NOT NULL,
     section_id INT NOT NULL,
-    grade ENUM('A', 'B', 'C', 'D', 'F', 'W'),
+    grade ENUM('A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'D+', 'D', 'D-', 'F', 'W'),
     status ENUM('failed', 'passed', 'dropped') NOT NULL,
     FOREIGN KEY (student_id) REFERENCES students(student_id)  ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (section_id) REFERENCES sections(section_id)  ON UPDATE CASCADE ON DELETE RESTRICT
