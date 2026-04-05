57b1a3fdcfe9dec5965e8cf6ab0e14974b15f60e
Khalia Dogan <kdogan@kent.edu>
2026-04-05 15:15:42 -0400

    Add 'W' grade option to enrollments table

--- db_design/schema.sql
+++ db_design/schema.sql
@@ -138,7 +138,7 @@ CREATE TABLE enrollments (
     enrollment_id INT AUTO_INCREMENT PRIMARY KEY,
     student_id INT NOT NULL,
     section_id INT NOT NULL,
-    grade ENUM('A', 'B', 'C', 'D', 'F'),
+    grade ENUM('A', 'B', 'C', 'D', 'F', 'W'),
     status ENUM('failed', 'passed', 'dropped') NOT NULL,
     FOREIGN KEY (student_id) REFERENCES students(student_id)  ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (section_id) REFERENCES sections(section_id)  ON UPDATE CASCADE ON DELETE RESTRICT
