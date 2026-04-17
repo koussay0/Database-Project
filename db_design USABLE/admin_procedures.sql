e3b45f2bd976a7ed48122e98663f50ae6178d957
bhullarravia <bhullarravia@gmail.com>
2026-04-16 20:52:53 -0400

    admin procedures

--- db_design USABLE/admin_procedures.sql
+++ db_design USABLE/admin_procedures.sql
@@ -68,13 +68,21 @@ CREATE PROCEDURE read_sections()
 BEGIN
     SELECT 
         sections.section_id, 
-        sections.course_id,
-        sections.classroom_id,
-        sections.timeslot_id,
+        courses.title,
+        courses.course_number,
+        buildings.name,
+        classrooms.room_number,
+        timeslots.day,
+        timeslots.start_time,
+        timeslots.end_time,
         sections.semester, 
         sections.year,
         sections.capacity
-    FROM sections;
+    FROM sections
+    JOIN courses ON sections.course_id = courses.course_id
+    LEFT JOIN classrooms ON sections.classroom_id = classrooms.classroom_id
+    LEFT JOIN buildings ON classrooms.building_id = buildings.building_id
+    LEFT JOIN timeslots ON sections.timeslot_id = timeslots.timeslot_id;
 END //
 
 CREATE PROCEDURE update_section(
@@ -102,9 +110,128 @@ END //
 
 -- CRUD Classroom
 
+CREATE PROCEDURE create_classroom (
+    building_id_n INT,
+    room_number_n VARCHAR(10),
+    capacity_n INT
+)
+BEGIN
+    INSERT INTO classrooms (building_id, room_number, capacity)
+    VALUES (building_id_n, room_number_n, capacity_n);
+END //
+
+CREATE PROCEDURE read_classroom()
+BEGIN
+    SELECT
+        classrooms.classroom_id,
+        buildings.name,
+        classrooms.room_number,
+        classrooms.capacity
+    FROM classrooms
+    JOIN buildings ON classrooms.building_id = buildings.building_id;
+END //
+
+CREATE PROCEDURE update_classroom(
+    classroom_id_n INT,
+    building_id_n INT,
+    room_number_n VARCHAR(10),
+    capacity_n INT
+)
+BEGIN
+    UPDATE classrooms
+    SET building_id = building_id_n, room_number = room_number_n, capacity = capacity_n
+    WHERE classroom_id = classroom_id_n;
+END //
+
+CREATE PROCEDURE delete_classroom (
+    IN classroom_id_n INT
+)
+BEGIN
+    DELETE FROM classrooms WHERE classroom_id = classroom_id_n;
+END //
+
+
 -- CRUD Department
 
+CREATE PROCEDURE create_department (
+    name_n VARCHAR(100),
+    budget_n DECIMAL(10, 2),
+    building_id_n INT
+)
+BEGIN
+    INSERT INTO departments (name, budget, building_id)
+    VALUES (name_n, budget_n, building_id_n);
+END //
+
+CREATE PROCEDURE read_department()
+BEGIN
+    SELECT
+        departments.dept_id,
+        departments.name,
+        departments.budget,
+        buildings.name
+    FROM departments
+    JOIN buildings ON departments.building_id = buildings.building_id;
+END //
+
+CREATE PROCEDURE update_department(
+    dept_id_n INT,
+    name_n VARCHAR(100),
+    budget_n DECIMAL(10, 2),
+    building_id_n INT
+)
+BEGIN
+    UPDATE departments
+    SET name = name_n, budget = budget_n, building_id = building_id_n
+    WHERE dept_id = dept_id_n;
+END //
+
+CREATE PROCEDURE delete_department (
+    IN dept_id_n INT
+)
+BEGIN
+    DELETE FROM departments WHERE dept_id = dept_id_n;
+END //
+
 -- CRUD Timeslot
+CREATE PROCEDURE create_timeslot(
+    IN day_n ENUM('M', 'T', 'W', 'R', 'F'),
+    IN start_time_n TIME,
+    IN end_time_n TIME
+)
+BEGIN
+    INSERT INTO timeslots (day, start_time, end_time)
+    VALUES (day_n, start_time_n, end_time_n);
+END //
+
+CREATE PROCEDURE read_timeslot()
+BEGIN
+    SELECT
+        timeslots.timeslot_id,
+        timeslots.day,
+        timeslots.start_time,
+        timeslots.end_time
+    FROM timeslots;
+END //
+
+CREATE PROCEDURE update_timeslot(
+    timeslot_id_n INT,
+    day_n ENUM('M', 'T', 'W', 'R', 'F'),
+    start_time_n TIME,
+    end_time_n TIME 
+)
+BEGIN
+    UPDATE timeslots
+    SET day = day_n, start_time = start_time_n, end_time = end_time_n
+    WHERE timeslot_id = timeslot_id_n;
+END //
+
+CREATE PROCEDURE delete_timeslot (
+    IN timeslot_id_n INT
+)
+BEGIN
+    DELETE FROM timeslots WHERE timeslot_id = timeslot_id_n;
+END //
 
 -- CRUD Instructor
 
@@ -131,7 +258,7 @@ BEGIN
         departments.name
     FROM instructors
     LEFT JOIN departments ON instructors.dept_id = departments.dept_id;
-END // 
+END //
 
 CREATE PROCEDURE update_instructor(
     IN instructor_id_n INT,
@@ -174,7 +301,7 @@ BEGIN
         students.first_name,
         students.last_name,
         students.email,
-        departments.name
+        departments.name 
     FROM students
     LEFT JOIN departments ON students.dept_id = departments.dept_id;
 END //
@@ -214,12 +341,13 @@ END //
 -- Modify
 CREATE PROCEDURE modify_instructor_assignment(
     IN instructor_id_n INT,
-    IN section_id_n INT
+    IN old_section_id_n INT,  
+    IN new_section_id_n INT
 )
 BEGIN
     UPDATE teaches
-    SET section_id = section_id_n
-    WHERE instructor_id = instructor_id_n;
+    SET section_id = new_section_id_n
+    WHERE instructor_id = instructor_id_n AND section_id = old_section_id_n;
 END //
 
 -- Remove
